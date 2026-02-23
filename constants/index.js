4d10c601b676daae1d5a9058e07c9df753a51b12
Vigneshwarrs <vicky09989@gmail.com>
2026-02-23 18:33:34 +0530

    Implemented Cocktail section

--- constants/index.js
+++ constants/index.js
@@ -9,11 +9,11 @@ const cocktailLists = [
   {
     name: "Chapel Hill Shiraz",
     country: "AU",
-    details: "Battle",
+    detail: "Battle",
     price: "$10",
   },
-  { name: "Caten Malbee", country: "AU", details: "Battle", price: "$49" },
-  { name: "Rhino Pale Ale", country: "CA", details: "750 ml", price: "$30" },
+  { name: "Caten Malbee", country: "AU", detail: "Battle", price: "$49" },
+  { name: "Rhino Pale Ale", country: "CA", detail: "750 ml", price: "$30" },
   { name: "Irish Guinness", country: "IE", detail: "600 ml", price: "$29" },
 ];
 
